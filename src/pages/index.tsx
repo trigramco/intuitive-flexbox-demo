afaeeb96428d32a675d5165572bcb2626316358c
Adam Murray <murray.r.adam@gmail.com>
2023-03-03 10:59:21 +1100

    feat: demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import * as React from 'react';
+import { HBox, VBox } from 'intuitive-flexbox'
+import * as React from 'react'
 
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import Seo from '@/components/Seo';
+import Layout from '@/components/layout/Layout'
+import Seo from '@/components/Seo'
 
 /**
  * SVGR Support
@@ -14,7 +11,6 @@ import Seo from '@/components/Seo';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -25,50 +21,20 @@ export default function HomePage() {
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
-
+      <VBox className='h-screen bg-black py-10 text-white'>
+        <h1 className='mb-2'>This is a VBox</h1>
+        <HBox className='bg-blue-500 bg-opacity-50 py-2'>
+          This is an HBox
+          <div className='mx-2 bg-red-500 px-2'>with</div>
+          <div className='mx-2 bg-green-500 px-2'>multiple items</div>
+        </HBox>
+        <HBox className='bg-yellow-500 bg-opacity-50'>This another HBox</HBox>
+      </VBox>
       <main>
         <section className='bg-white'>
-          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
-
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
-            </footer>
-          </div>
+          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'></div>
         </section>
       </main>
     </Layout>
-  );
+  )
 }
